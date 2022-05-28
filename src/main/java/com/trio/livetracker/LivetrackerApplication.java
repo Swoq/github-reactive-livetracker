b46fed4aef9b7aa8cc90e70ead206a2a9a1bcd6a
Swoqe <misha.kratyuk@gmail.com>
2022-05-28 16:44:15 +0300

    Spring initial

---
+++ src/main/java/com/trio/livetracker/LivetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.trio.livetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivetrackerApplication.class, args);
+	}
+
+}
